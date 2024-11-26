471237059b30a152cc2b3d30e93056587aae7f99
Nikita Yeldynov <yeldynov@gmail.com>
2024-11-26 18:20:26 +0200

    + font

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <main>
-      <h1 className='text-3xl font-bold underline'>Hello Cyprus!</h1>
-    </main>
+    <div className='font-lato bg-[#EBF5FB] min-h-screen'>
+      <main className='mx-auto max-w-7xl'>
+        <h1 className='text-3xl font-bold underline'>Hello Cyprus!</h1>
+      </main>
+    </div>
   )
 }
 
