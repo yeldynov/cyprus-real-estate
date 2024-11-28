132665e1465f763b9274232e98579672ad66d312
Nikita Yeldynov <yeldynov@gmail.com>
2024-11-28 16:42:21 +0200

    * heading

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,7 +6,7 @@ type HeadingProps = {
 const Heading = ({ left, right }: HeadingProps) => {
   return (
     <div className='flex flex-col justify-between w-full lg:gap-10 lg:flex-row'>
-      <h2 className='lg:text-[32px] text-center lg:text-left text-xl font-bold'>
+      <h2 className='lg:text-[32px] lg:mt-4 text-center lg:text-left text-xl font-bold'>
         {left}
       </h2>
       <p className='flex-1 hidden mt-3 text-2xl text-right border-b lg:block text-secondaryBlue border-secondaryBlue '>
