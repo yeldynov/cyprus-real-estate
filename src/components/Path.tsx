672bf9a7e3d3faa47388341824555fc3232b68a4
Nikita Yeldynov <yeldynov@gmail.com>
2024-11-30 15:12:34 +0200

    + investment options

--- src/components/Path.tsx
+++ src/components/Path.tsx
@@ -39,6 +39,75 @@ const Path = () => {
         left='Investment Options for Cyprus Residency'
         right='Choose Your Investment Path'
       />
+      {/* Investment options */}
+      <div className='mt-5 lg:mt-20'>
+        <div className='flex flex-col w-full py-5 border-b lg:flex-row'>
+          <h3 className='pr-10 mb-4 text-base font-semibold lg:text-2xl lg:w-1/3'>
+            Investment in Residential Property
+          </h3>
+          <div className='flex flex-col gap-4 lg:w-2/3'>
+            <h4 className='text-sm font-semibold lg:text-xl'>
+              Secure Residency with a Home Purchase
+            </h4>
+            <p className='text-sm lg:text-base'>
+              Purchase up to two new housing units (apartments or houses) in
+              Cyprus, with a total value of at least €300,000 plus VAT. The
+              properties must be purchased directly from the development
+              company, and the funds must be transferred from abroad. If the
+              property is sold, residency status is forfeited but can be
+              retained by reinvesting in a property of equal or greater value
+            </p>
+          </div>
+        </div>
+        <div className='flex flex-col w-full py-5 border-b lg:flex-row'>
+          <h3 className='pr-10 mb-4 text-base font-semibold lg:text-2xl lg:w-1/3'>
+            Investment in Commercial Real Estate
+          </h3>
+          <div className='flex flex-col gap-4 lg:w-2/3'>
+            <h4 className='text-sm font-semibold lg:text-xl'>
+              Diverse Commercial Real Estate Options
+            </h4>
+            <p className='text-sm lg:text-base'>
+              Invest in offices, shops, hotels, or a mix of commercial
+              properties, with a total minimum investment of €300,000. Both new
+              and resale commercial properties are eligible, providing
+              flexibility in investment types
+            </p>
+          </div>
+        </div>
+        <div className='flex flex-col w-full py-5 border-b lg:flex-row'>
+          <h3 className='pr-10 mb-4 text-base font-semibold lg:text-2xl lg:w-1/3'>
+            Investment in a Cypriot Business
+          </h3>
+          <div className='flex flex-col gap-4 lg:w-2/3'>
+            <h4 className='text-sm font-semibold lg:text-xl'>
+              Ownership in Local Businesses
+            </h4>
+            <p className='text-sm lg:text-base'>
+              Invest in the share capital of a Cypriot business with active
+              operations and staff based in Cyprus. The business must employ at
+              least five people and maintain a physical presence on the island,
+              showcasing your commitment to the local economy
+            </p>
+          </div>
+        </div>
+        <div className='flex flex-col w-full py-5 border-b lg:flex-row'>
+          <h3 className='pr-10 mb-4 text-base font-semibold lg:text-2xl lg:w-1/3'>
+            Investment in Cypriot Collective Funds
+          </h3>
+          <div className='flex flex-col gap-4 lg:w-2/3'>
+            <h4 className='text-sm font-semibold lg:text-xl'>
+              Alternative Investment Opportunities
+            </h4>
+            <p className='text-sm lg:text-base'>
+              Invest in shares of Cypriot Collective Investment Organizations
+              (such as AIF, AIFLNP, RAIF) to meet residency requirements. This
+              option allows for flexibility in asset management while
+              contributing to Cyprus's economic landscape
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
